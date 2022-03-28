f1b019adb9b12b74d184b9a9756b3d0ff2549dcf
Albin05 <97468317+Albin05@users.noreply.github.com>
2022-03-28 10:52:38 +0530

    Update authenticate.js

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -3,6 +3,13 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = (token) => {
     return new Promise((resolve, reject) => {
-        jwt.verify(token, proces.env.SECRET_KEY)
+        jwt.verify(token, proces.env.SECRET_KEY, (err,decoded) => {
+            if(err) return reject(err)
+
+            return resolve(decoded)
+        });
     })
 }
+
+
+const authenticate = async (req, res)
